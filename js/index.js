508963f3e0bced78133c3e791a2369324eb671f9
suyarkhonuzb <suyarxonuzb007@gmail.com>
2023-08-13 23:43:31 +0500

    Add index countries

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 const elSiteThemeToggle = document.querySelector('.button-dark-toggle');
 elSiteThemeToggle.addEventListener('click', function(){
-    document.body.classList.add('dark-mode');
+    document.body.classList.toggle('dark-mode');
 })
